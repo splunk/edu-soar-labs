79b1b925d883709eff86902b17b7b60131f360c3
Administrator <changeme@example.com>
2026-08-20 00:16:59 +0000

    add parameter to REST API URI so that all closed containers are retrieved, not just 10

--- air_advanced_solutions/bb_Solution_Adv_Lab_7 | Clean Closed Containers.py
+++ air_advanced_solutions/bb_Solution_Adv_Lab_7 | Clean Closed Containers.py
@@ -5,23 +5,23 @@ import phantom.rules as phantom
 def clean_closed_containers():
     # deletes all finding containers in status_name == "Closed"
         
-    containers_url = phantom.build_phantom_rest_url('container') + "?_filter_data__status_name=\"Closed\""
-    response = phantom.requests.get(containers_url, verify=False)    
+    containers_url = phantom.build_phantom_rest_url('container') + "?page_size=0&_filter_data__status_name=\"Closed\""
+    response = phantom.requests.get(containers_url, verify=False)
     containers = response.json().get('data', [])
     
     if len(containers)==0:
-        phantom.debug("Did not find any closed findings")
+        phantom.debug("Did not find any closed containers")
         return
     
-    phantom.debug(f"Found {len(containers)} closed findings, deleting...")
+    phantom.debug(f"Found {len(containers)} closed containers, deleting...")
     
     for container in containers:
         delete_url = phantom.build_phantom_rest_url('container', container["id"])
-        delete_response = phantom.requests.delete(delete_url, verify=False)        
+        delete_response = phantom.requests.delete(delete_url, verify=False)
 
 def on_start(container):
-    phantom.debug('on_start() called')    
-    clean_closed_containers() 
+    phantom.debug('on_start() called')
+    clean_closed_containers()
 
 def on_finish(container, summary):
     phantom.debug("on_finish() called")
